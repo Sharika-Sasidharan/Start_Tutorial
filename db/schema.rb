b1566fe36fdd3c25b50879310b477407d53d37d8
EC2 Default User <ec2-user@ip-172-31-7-190.us-east-2.compute.internal>
2018-09-14 06:53:07 +0000

    Added plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_155628) do
+ActiveRecord::Schema.define(version: 2018_09_05_155629) do
 
-  create_table "contacts", force: :cascade do |t|
+  create_table "Contacts", force: :cascade do |t|
     t.string "Name"
     t.string "Email"
     t.text "Comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
